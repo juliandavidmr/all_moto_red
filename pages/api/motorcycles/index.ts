568288bf3ba0c3c8bcecc6d57914bda345c4a98d
juliandavidmr <julian.mora@crehana.com>
2021-04-24 19:11:48 -0500

    chore: clean project

--- pages/api/motorcycles/index.ts
+++
@@ -1,7 +0,0 @@
-import { normalizeJSON } from "../../../utils/stringify";
-import { getPopularMotorcycles } from "../../../lib/db/motorcycle";
-
-export default async (req, res) => {
-	const motorcycles = await getPopularMotorcycles();
-	res.status(200).json({ data: normalizeJSON(motorcycles) })
-}
--- utils/stringify.ts
+++
@@ -1,7 +0,0 @@
-export function normalizeJSON(str: any) {
-	return JSON.parse(JSON.stringify(str, (key, value) =>
-		typeof value === 'bigint'
-			? value.toString()
-			: value // return everything else unchanged
-	));
-}
